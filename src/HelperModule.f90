7939c80dc86381bb9f462e6e6feb7380703f2a55
eml11 <eml11@ic.ac.uk>
2013-10-27 20:31:03 +0000

    Added HelperModule License

--- src/HelperModule.f90
+++ src/HelperModule.f90
@@ -1,7 +1,35 @@
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!  
+!  geotherm.
+!  
+!  Copyright (C) 2013 Elliot Lynch.
+!  
+!  Elliot M. Lynch, eml11@ic.ac.uk
+!  Department of Earth Science and Engineering
+!  Imperial College London
+!  
+!  This library is free software; you can redistribute it and/or
+!  modify it under the terms of the GNU Lesser General Public
+!  License as published by the Free Software Foundation,
+!  version 2.1 of the License.
+!  
+!  This library is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!  Lesser General Public License for more details.
+!  
+!  You should have received a copy of the GNU Lesser General Public
+!  License along with this library; if not, write to the Free Software
+!  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+!  USA
+!  
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
       module HelperModule
       use netcdf
       implicit none
-!234567
+
       contains
 
       subroutine get_netcdf(filename,data_ar)
