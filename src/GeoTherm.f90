f71273a861818dcdab338c9ca9dc86ff5c0d5906
eml11 <eml11@ic.ac.uk>
2013-10-30 01:23:47 +0000

    Initial comments to main program

--- src/GeoTherm.f90
+++ src/GeoTherm.f90
@@ -1,4 +1,3 @@
-
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !  
 !  geotherm.
@@ -35,9 +34,16 @@
       type (modelfile) modelfile_inst
       character (len = 256) :: filename
 
+      !gets the name of the file use to specify
+      !model parameters, set as a sytem argument
       call GETARG(1,filename)
 
+      !reads the file used to specify model parameters
+      !and stores these in the instace modelfile_inst
+      ! of derived type modelfile
       call READMDLF(modelfile_inst,filename)
+      
+      !main work subroutine
       call compute_geotherm &
       &(modelfile_inst,modelfile_inst%ydim,modelfile_inst%tdim)
 
@@ -67,6 +73,7 @@
       double precision :: iner_dbl
       double precision :: outr_dbl
 
+      !reading netcdfs from files sepcified in the modelfile
       call get_netcdf1d(modelfile_inst%gtempfile,input_tdata_ar)
       call get_netcdf1d(modelfile_inst%gqfluxfile,input_qdata_ar)
       call get_netcdf_wincrmnt &
@@ -77,35 +84,53 @@
       call get_netcdf &
       &(modelfile_inst%thermlconductnetcdf,thermlconduct_ar)
 
-      !print *, incriment
-
+      !this is a hack to fix issue with netcdfs created by gmt 
       incriment(2) = -incriment(2)
 
+      !extend 1d netcdfs into 2d by copying along 2d dimension
       call extend_ardimension(input_tdata_ar,tdata_artrs,n)
       call extend_ardimension(input_qdata_ar,qdata_artrs,n)
       tdata_ar = RESHAPE(tdata_artrs,(/n,m/))
       qdata_ar = RESHAPE(qdata_artrs,(/n,m/))
 
+      !creates array corrisponding to b(t)'s time differential
       call compute_bdashval &
       &(tdata_ar,qdata_ar,thermlconduct_ar, &
       &bdash_ar,incriment,n,m)
+      
+      !creates array corrisponding to the function which appears
+      !in the exponent
       call compute_exponentintegral &
       &(bdash_ar,velocity_ar,thermlconduct_ar/(density_ar*heatcapc_ar),&
       &exintegral_ar,incriment,n,m)
+      
+      !creates array corrisponding to the first integral with respect
+      !to eta
       call compute_init_inerintegral &
       &(exintegral_ar,bdash_ar,density_ar*heatproduct_ar,&
       &thermlconduct_ar,inerintegral_ar,incriment,n,m)
+      
+      !creates a double which is the integration constant of the
+      !integral calculated in the above subroutine required to set
+      !the surface heatflux equal to the qdata_ar
       call compute_inerintegralconstant &
       &(inerintegral_ar,exintegral_ar,tdata_ar,qdata_ar, &
       &bdash_ar,thermlconduct_ar,iner_dbl,incriment,n,m)
+      
+      !creates array corrisponding to the second integral with respect
+      !to eta
       call compute_init_outerintegral &
       &(inerintegral_ar,exintegral_ar,iner_dbl, &
       &outerintegral_ar,incriment,n,m)
+      
+      !creates double which is the integration constant of the
+      !integral calcultaed in the above subroutine required to set
+      !the surface Temperature to tdata_ar
       call compute_outerintegralconstant &
       &(outerintegral_ar,tdata_ar,outr_dbl,n,m)
 
+      !writes output netcdf
       call write_netcdf &
       &(modelfile_inst%outfile,outerintegral_ar+outr_dbl,n,m)
 
       end subroutine
-
